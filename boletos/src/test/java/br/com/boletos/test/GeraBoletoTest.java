b390da7bc4519852fb19a69cc0cb26ca86afb67d
Rafael Batista <rafaelbs@ciandt.com>
2016-07-13 11:26:53 -0300

    [rabsouza] Ajuste na geracao e na exportacao

--- boletos/src/test/java/br/com/boletos/test/GeraBoletoTest.java
+++ boletos/src/test/java/br/com/boletos/test/GeraBoletoTest.java
@@ -18,124 +18,89 @@ import br.com.caelum.stella.boleto.transformer.BoletoGenerator;
 
 public class GeraBoletoTest {
 
-	private static final int DIA_VENC_BOLETO = 20;
-	private static final int DIA_DOC_BOLETO = 5;
-	private static final int MES_BOLETO = 12;
-	private static final int ANO_BOLETO = 2015;
+    private static final int DIA_VENC_BOLETO = 20;
+    private static final int DIA_DOC_BOLETO = 5;
+    private static final int MES_BOLETO = 7;
+    private static final int ANO_BOLETO = 2016;
 
-	private static final String base_path = "export-" + DIA_VENC_BOLETO + "-" + MES_BOLETO + "-" + ANO_BOLETO + "-";
+    private static final String base_path = "export/export-" + DIA_VENC_BOLETO + "-" + MES_BOLETO + "-" + ANO_BOLETO + "-";
 
-	@Test
-	public void geraBoleloRafael() {
-		Random rand = new Random();
+    @Test
+    public void geraBoleloRafael() {
+        Random rand = new Random();
 
-		Datas datas = Datas.newDatas().withDocumento(DIA_DOC_BOLETO, MES_BOLETO, ANO_BOLETO)
-				.withProcessamento(DIA_DOC_BOLETO, MES_BOLETO, ANO_BOLETO).withVencimento(DIA_VENC_BOLETO, MES_BOLETO, ANO_BOLETO);
+        Datas datas = Datas.newDatas().withDocumento(DIA_DOC_BOLETO + rand.nextInt(5), MES_BOLETO, ANO_BOLETO).withProcessamento(DIA_DOC_BOLETO + rand.nextInt(5), MES_BOLETO, ANO_BOLETO)
+                        .withVencimento(DIA_VENC_BOLETO, MES_BOLETO, ANO_BOLETO);
 
-		Emissor emissor = Emissor
-				.newEmissor()
-				.withCedente(
-						"Centro Federal de Educação Tecnológica - FUNEC ")
-				.withAgencia(1615).withDvAgencia('2').withContaCorrente(333003)
-				.withDvContaCorrente('6').withCarteira(18)
-				.withNossoNumero(Long.valueOf(+(rand.nextInt(90000) + 90000) + "9854" + (rand.nextInt(90000) + 90000)));
+        Emissor emissor = Emissor.newEmissor().withCedente("Centro Federal de Educação Tecnológica - FUNEC ").withAgencia(1615).withDvAgencia('2').withContaCorrente(333003).withDvContaCorrente('6')
+                        .withCarteira(18).withNossoNumero(Long.valueOf(+(rand.nextInt(90000) + 90000) + "9854" + (rand.nextInt(90000) + 90000)));
 
-		Sacado sacado = Sacado.newSacado()
-				.withNome("RAFAEL ALESSANDRO BATISTA")
-				.withCpf("         CPF: 079.667.236-90")
-				.withEndereco("Rua Guajajaras, 329 Ap 306")
-				.withBairro("Centro").withCep("30180-100")
-				.withCidade("Belo Horizonte").withUf("MG");
+        Sacado sacado = Sacado.newSacado().withNome("RAFAEL ALESSANDRO BATISTA").withCpf("         CPF: 079.667.236-90").withEndereco("Rua Guajajaras, 329 Ap 306").withBairro("Centro")
+                        .withCep("30180-100").withCidade("Belo Horizonte").withUf("MG");
 
-		Banco banco = new BancoDoBrasil();
+        Banco[] banco = { new BancoDoBrasil() };
+        int indexBanco = (int) (Math.round(Math.random()) % banco.length);
 
-		DateFormat formatter = new SimpleDateFormat("MMM");
-		GregorianCalendar calendar = new GregorianCalendar();
-		calendar.set(Calendar.MONTH, MES_BOLETO - 1);
+        DateFormat formatter = new SimpleDateFormat("MMM");
+        GregorianCalendar calendar = new GregorianCalendar();
+        calendar.set(Calendar.MONTH, MES_BOLETO - 1);
 
-		Boleto boleto = Boleto
-				.newBoleto()
-				.withBanco(banco)
-				.withDatas(datas)
-				.withEmissor(emissor)
-				.withSacado(sacado)
-				.withValorBoleto("350.00")
-				.withInstrucoes(
-						"Contribuinte: RAFAEL ALESSANDRO BATISTA " + formatter.format(calendar.getTime()) + "/ " + ANO_BOLETO,
-						"CPF/CNPJ: 079.667.236-90", " ",
-						"Unidade: 30 - COLEGIADO DA DIRETORIA",
-						"Tipo Pagamento: 165 - DPCT-TÉCNICO EM ADMINISTRAÇÃO")
-				.withLocaisDePagamento(
-						"ATÉ O VENCIMENTO, PAGÁVEL EM QUALQUER AGÊNCIA DA REDE BANCÁRIA")
-				.withNoDocumento("0" + (rand.nextInt(90000) + 90000));
+        Boleto boleto = Boleto.newBoleto().withBanco(banco[indexBanco]).withDatas(datas).withEmissor(emissor).withSacado(sacado).withValorBoleto("350.00")
+                        .withInstrucoes("Contribuinte: RAFAEL ALESSANDRO BATISTA " + formatter.format(calendar.getTime()) + "/ " + ANO_BOLETO, "CPF/CNPJ: 079.667.236-90", " ",
+                                        "Unidade: 30 - COLEGIADO DA DIRETORIA", "Tipo Pagamento: 165 - DPCT-TÉCNICO EM ADMINISTRAÇÃO")
+                        .withLocaisDePagamento("ATÉ O VENCIMENTO, PAGÁVEL EM QUALQUER AGÊNCIA DA REDE BANCÁRIA").withNoDocumento("0" + (rand.nextInt(90000) + 90000));
 
-		BoletoGenerator gerador = new BoletoGenerator(boleto);
+        BoletoGenerator gerador = new BoletoGenerator(boleto);
 
-		// Para gerar um boleto em PDF
-		System.out.println("Path: "+ base_path + "BancoDoBrasilRafael.pdf");
-		gerador.toPDF(base_path + "BancoDoBrasilRafael.pdf");
+        // Para gerar um boleto em PDF
+        String boletoName = "BoletoRafael.pdf";
+        System.out.println("Path: " + base_path + boletoName);
+        gerador.toPDF(base_path + boletoName);
 
-		// Para gerar um boleto em PNG
-		System.out.println("Path: "+ base_path + "BancoDoBrasilRafael.png");
-		gerador.toPNG(base_path + "BancoDoBrasilRafael.png");
+        // Para gerar um boleto em PNG
+        String boletoImg = "ImgRafael.png";
+        System.out.println("Path: " + base_path + boletoImg);
+        gerador.toPNG(base_path + boletoImg);
 
+    }
 
-	}
+    @Test
+    public void geraBoleloVanessa() {
+        Random rand = new Random();
 
-	@Test
-	public void geraBoleloVanessa() {
-		Random rand = new Random();
+        Datas datas = Datas.newDatas().withDocumento(DIA_DOC_BOLETO + rand.nextInt(5), MES_BOLETO, ANO_BOLETO).withProcessamento(DIA_DOC_BOLETO + rand.nextInt(5), MES_BOLETO, ANO_BOLETO)
+                        .withVencimento(DIA_VENC_BOLETO, MES_BOLETO, ANO_BOLETO);
 
-		Datas datas = Datas.newDatas().withDocumento(DIA_DOC_BOLETO, MES_BOLETO, ANO_BOLETO)
-				.withProcessamento(DIA_DOC_BOLETO, MES_BOLETO, ANO_BOLETO).withVencimento(DIA_VENC_BOLETO, MES_BOLETO, ANO_BOLETO);
+        Emissor emissor = Emissor.newEmissor().withCedente("Centro Federal de Educação Tecnológica - FUNEC ").withAgencia(1615).withDvAgencia('2').withContaCorrente(4578451).withDvContaCorrente('6')
+                        .withCarteira(18).withNossoNumero(Long.valueOf(+(rand.nextInt(90000) + 90000) + "8621" + (rand.nextInt(90000) + 90000)));
 
-		Emissor emissor = Emissor
-				.newEmissor()
-				.withCedente(
-						"Centro Federal de Educação Tecnológica - FUNEC ")
-				.withAgencia(1615).withDvAgencia('2').withContaCorrente(4578451)
-				.withDvContaCorrente('6').withCarteira(18)
-				.withNossoNumero(Long.valueOf(+(rand.nextInt(90000) + 90000) + "8621" + (rand.nextInt(90000) + 90000)));
+        Sacado sacado = Sacado.newSacado().withNome("VANESSA PARREIRAS DE CASTRO").withCpf("         CPF: 046.095.776-74").withEndereco("Rua Sandra, 108 ").withBairro("Nova Cintra")
+                        .withCep("30513-270").withCidade("Belo Horizonte").withUf("MG");
 
-		Sacado sacado = Sacado.newSacado()
-				.withNome("VANESSA PARREIRAS DE CASTRO")
-				.withCpf("         CPF: 046.095.776-74")
-				.withEndereco("Rua Sandra, 108 ")
-				.withBairro("Nova Cintra").withCep("30513-270")
-				.withCidade("Belo Horizonte").withUf("MG");
+        Banco[] banco = { new BancoDoBrasil() };
+        int indexBanco = (int) (Math.round(Math.random()) % banco.length);
 
-		Banco banco = new BancoDoBrasil();
+        DateFormat formatter = new SimpleDateFormat("MMM");
+        GregorianCalendar calendar = new GregorianCalendar();
+        calendar.set(Calendar.MONTH, MES_BOLETO - 1);
 
-		DateFormat formatter = new SimpleDateFormat("MMM");
-		GregorianCalendar calendar = new GregorianCalendar();
-		calendar.set(Calendar.MONTH, MES_BOLETO - 1);
+        Boleto boleto = Boleto.newBoleto().withBanco(banco[indexBanco]).withDatas(datas).withEmissor(emissor).withSacado(sacado).withValorBoleto("330.00")
+                        .withInstrucoes("Contribuinte: VANESSA PARREIRAS DE CASTRO " + formatter.format(calendar.getTime()) + "/ " + ANO_BOLETO, "CPF/CNPJ: 046.095.776-74", " ",
+                                        "Unidade: 30 - COLEGIADO DA DIRETORIA", "Tipo Pagamento: 135 - DPCT-TÉCNICO EM GERH")
+                        .withLocaisDePagamento("ATÉ O VENCIMENTO, PAGÁVEL EM QUALQUER AGÊNCIA DA REDE BANCÁRIA").withNoDocumento("0" + (rand.nextInt(90000) + 90000));
 
-		Boleto boleto = Boleto
-				.newBoleto()
-				.withBanco(banco)
-				.withDatas(datas)
-				.withEmissor(emissor)
-				.withSacado(sacado)
-				.withValorBoleto("330.00")
-				.withInstrucoes(
-						"Contribuinte: VANESSA PARREIRAS DE CASTRO " + formatter.format(calendar.getTime()) + "/ " + ANO_BOLETO,
-						"CPF/CNPJ: 046.095.776-74", " ",
-						"Unidade: 30 - COLEGIADO DA DIRETORIA",
-						"Tipo Pagamento: 135 - DPCT-TÉCNICO EM GERH")
-				.withLocaisDePagamento(
-						"ATÉ O VENCIMENTO, PAGÁVEL EM QUALQUER AGÊNCIA DA REDE BANCÁRIA")
-				.withNoDocumento("0" + (rand.nextInt(90000) + 90000));
+        BoletoGenerator gerador = new BoletoGenerator(boleto);
 
-		BoletoGenerator gerador = new BoletoGenerator(boleto);
+        // Para gerar um boleto em PDF
+        String boletoName = "BoletoVanessa.pdf";
+        System.out.println("Path: " + base_path + boletoName);
+        gerador.toPDF(base_path + boletoName);
 
-		// Para gerar um boleto em PDF
-		System.out.println("Path: "+ base_path + "BancoDoBrasilVanessa.pdf");
-		gerador.toPDF(base_path + "BancoDoBrasilVanessa.pdf");
+        // Para gerar um boleto em PNG
+        String boletoImg = "ImgVanessa.png";
+        System.out.println("Path: " + base_path + boletoImg);
+        gerador.toPNG(base_path + boletoImg);
 
-		// Para gerar um boleto em PNG
-		System.out.println("Path: "+ base_path + "BancoDoBrasilVanessa.png");
-		gerador.toPNG(base_path + "BancoDoBrasilVanessa.png");
-
-	}
+    }
 
 }
